e72e7f80be46e2905b0a9980ef54cd9270f2b72e
KapaRajashekarReddy-Quadrant <rajashekar.reddy@quadranttechnologies.com>
2026-06-12 20:40:25 +0530

    Update ConnectSnowflakeModal.tsx

--- src/components/modals/SnowflakeToDatabricks/ConnectSnowflakeModal.tsx
+++ src/components/modals/SnowflakeToDatabricks/ConnectSnowflakeModal.tsx
@@ -97,7 +97,7 @@ export function ConnectSnowflakeModal({ open, onClose, onConnect }: ConnectSnowf
     };
 
     try {
-      const response = await fetch("https://20.106.196.248:8443/api/discover-snowflake", {
+      const response = await fetch("https://snowflaketodatabricks-h2bgh2hch9h7f6fb.z01.azurefd.net/api/discover-snowflake", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(requestPayload),
